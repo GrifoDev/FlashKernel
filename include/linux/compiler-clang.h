c44d1b1b96f50c15cdd00e41efd0dbb619e544cb
Sami Tolvanen <samitolvanen@google.com>
2017-05-11 15:03:36 -0700

    add support for clang Control Flow Integrity (CFI)
    This change adds the CONFIG_CFI_CLANG option, CFI error handling,
    and a faster look-up table for cross module CFI checks.
    
    Bug: 67506682
    Change-Id: Ic009f0a629b552a0eb16e6d89808c7029e91447d
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>
    Signed-off-by: Tkkg1994 <luca@grifodev.ch>

--- include/linux/compiler-clang.h
+++ include/linux/compiler-clang.h
@@ -22,6 +22,9 @@
 	__attribute__((__section__(".text..ftrace")))
 #endif
 
+#define __nocfi		__attribute__((no_sanitize("cfi")))
+#endif
+
 /* Clang doesn't have a way to turn it off per-function, yet. */
 #ifdef __noretpoline
 #undef __noretpoline
