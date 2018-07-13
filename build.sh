b41bcbe118c8a91ec1ed288811bc9225d6fa69ec
Tkkg1994 <luca@grifodev.ch>
2018-07-13 18:58:14 +0200

    build: toolchain update to linaro 7.3.1

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 export MODEL=star2lte
 export VARIANT=eur
 export ARCH=arm64
-export BUILD_CROSS_COMPILE=../Toolchain/gcc-linaro-7.2.1_aarch64-linux-gnu/bin/aarch64-linux-gnu-
+export BUILD_CROSS_COMPILE=../Toolchain/gcc-linaro-7.3.1_aarch64-linux-gnu/bin/aarch64-linux-gnu-
 export BUILD_JOB_NUMBER=`grep processor /proc/cpuinfo|wc -l`
 
 RDIR=$(pwd)
